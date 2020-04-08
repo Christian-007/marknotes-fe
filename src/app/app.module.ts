79338f8bdf5b037212195d9a2c44ed88449a563c
Christian Ing Sunardi <christian.sunardi@amarbank.co.id>
2020-04-08 20:45:40 +0700

    feat(topbar): add a topbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopbarModule } from './shared/components/topbar/topbar.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, TopbarModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
