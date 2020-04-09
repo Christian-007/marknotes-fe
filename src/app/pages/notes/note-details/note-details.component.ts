5022e1098bacc18fc52ca2538c0046fa47fbdd7e
Christian Ing Sunardi <christian.sunardi@amarbank.co.id>
2020-04-09 21:27:25 +0700

    feat(note-details): experimenting with marked, highlightjs and dompurify

--- src/app/pages/notes/note-details/note-details.component.ts
+++ src/app/pages/notes/note-details/note-details.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { SafeHtml, DomSanitizer } from '@angular/platform-browser';
+
+import * as marked from 'marked';
+import * as highlightjs from 'highlight.js';
+import * as DOMPurify from 'dompurify';
 
 @Component({
   selector: 'app-note-details',
   templateUrl: './note-details.component.html',
-  styleUrls: ['./note-details.component.scss']
+  styleUrls: ['./note-details.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class NoteDetailsComponent implements OnInit {
+  text: string;
+  data: SafeHtml;
+  md: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private sanitizer: DomSanitizer) {
+    const renderer = new marked.Renderer();
+    renderer.code = this.highlightCode;
+    this.md = marked.setOptions({ renderer });
+    this.text =
+      '# Hello Markdown\n```ts\nimport { Providers } from "@providers/provider";\n\nexport class Encryptor {\n  encryptionMethod: string; \n}\n```';
   }
 
+  ngOnInit() {
+    this.data = this.markdownToSafeHtml(this.text);
+  }
+
+  markdownToSafeHtml(markdownTexts: string): SafeHtml {
+    const html = this.md(markdownTexts);
+    const safeHtml = DOMPurify.sanitize(html);
+    return this.sanitizer.bypassSecurityTrustHtml(safeHtml);
+  }
+
+  highlightCode(code: string, language: string): string {
+    if (!(language && highlightjs.getLanguage(language))) {
+      // use 'markdown' as default language
+      language = 'markdown';
+    }
+
+    const result = highlightjs.highlight(language, code).value;
+    return `<code class="hljs ${language}">${result}</code>`;
+  }
 }
