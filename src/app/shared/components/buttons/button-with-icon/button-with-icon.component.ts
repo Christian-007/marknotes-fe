c8690ecd8a29669cd42fb42a018f0445eb6b4d3e
Christian Ing Sunardi <christian.sunardi@amarbank.co.id>
2020-04-15 21:11:14 +0700

    feat(button-with-icon): implement checked className stylings

--- src/app/shared/components/buttons/button-with-icon/button-with-icon.component.ts
+++ src/app/shared/components/buttons/button-with-icon/button-with-icon.component.ts
@@ -6,8 +6,9 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     <button
       type="button"
       class="BtnIcon btn d-flex align-items-center"
+      [ngClass]="getClassName()"
       [attr.aria-label]="icon"
-      (click)="buttonClick.emit()"
+      (click)="onButtonClick()"
     >
       <i aria-hidden="true" class="material-icons">{{ icon }}</i>
     </button>
@@ -18,8 +19,25 @@ export class ButtonWithIconComponent {
   @Input() icon: string;
   @Output() buttonClick: EventEmitter<any>;
 
+  private checked: boolean;
+
   constructor() {
     this.icon = 'not_interested';
     this.buttonClick = new EventEmitter();
+    this.checked = false;
+  }
+
+  onButtonClick(): void {
+    this.checked = !this.checked;
+
+    this.buttonClick.emit();
+  }
+
+  getClassName(): string {
+    if (this.checked) {
+      return `Active`;
+    }
+
+    return null;
   }
 }
