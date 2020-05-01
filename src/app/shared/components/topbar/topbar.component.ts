71aa9ddc8aeb58159de3ac16c736bc48408490f9
Christian Ing Sunardi <christian.sunardi@amarbank.co.id>
2020-05-01 11:01:36 +0700

    feat(topbar): read note title from ngrx/store

--- src/app/shared/components/topbar/topbar.component.ts
+++ src/app/shared/components/topbar/topbar.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 
 import * as fromRoot from 'src/app/pages/notes/shared/reducers';
 import { NavigationsActions } from 'src/app/pages/notes/shared/actions';
+import { INote } from 'src/app/shared/services/store/markdown-state.model';
 
 @Component({
   selector: 'app-topbar',
@@ -12,9 +13,11 @@ import { NavigationsActions } from 'src/app/pages/notes/shared/actions';
 })
 export class TopbarComponent implements OnInit {
   isPreview$: Observable<boolean>;
+  activeNote$: Observable<INote>;
 
   constructor(private store: Store<fromRoot.ApplicationState>) {
     this.isPreview$ = store.pipe(select(fromRoot.selectIsPreview));
+    this.activeNote$ = store.pipe(select(fromRoot.selectActiveNote));
   }
 
   ngOnInit() {}
