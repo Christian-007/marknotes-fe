81010725e627115245dd30550ec6ef74265330bd
Christian Ing Sunardi <christianingsunardi@gmail.com>
2020-06-07 14:14:54 +0700

    test(local-storage-strategy): add unit tests for create, delete, update

--- src/app/shared/services/storage-strategy/local-storage-strategy.spec.ts
+++ src/app/shared/services/storage-strategy/local-storage-strategy.spec.ts
@@ -6,9 +6,11 @@ import { LocalStorageStrategy } from '@app/shared/services/storage-strategy/loca
 import { StorageStrategy } from '@app/shared/services/storage-strategy/storage-strategy';
 import { EStorageStrategy } from '@app/shared/enums/strategy.enum';
 import { INote } from '@app/shared/models/markdown-state.model';
+import { LOCAL_STORAGE } from '@app/shared/constants/storage-name.const';
 
 describe('LocalStorageStrategy', () => {
   let localStorageStrategy: StorageStrategy;
+  let mockNote: INote;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -16,6 +18,13 @@ describe('LocalStorageStrategy', () => {
     });
 
     localStorageStrategy = TestBed.get(LocalStorageStrategy);
+    mockNote = {
+      id: '1',
+      dateCreated: 1590045443715,
+      htmlText: '',
+      markdownText: '## Some Markdown',
+      title: 'Test Title',
+    };
   });
 
   it('should create', () => {
@@ -27,15 +36,7 @@ describe('LocalStorageStrategy', () => {
   });
 
   it('should return array of notes, when load, and storage has some notes', () => {
-    const mockNotesData: INote[] = [
-      {
-        id: '1',
-        dateCreated: 1590045443715,
-        htmlText: '',
-        markdownText: '## Some Markdown',
-        title: 'Test Title',
-      },
-    ];
+    const mockNotesData: INote[] = [mockNote];
     const expected$ = cold('c', { c: mockNotesData });
 
     const spy = spyOn(localStorage, 'getItem');
@@ -77,6 +78,172 @@ describe('LocalStorageStrategy', () => {
       () => {},
       error => {
         expect(error.message).toBe(mockErrorMessage);
+      },
+    );
+  });
+
+  it('should call localStorage.setItem, with new note only, when create, with no loaded notes', () => {
+    const setItemSpy = spyOn(localStorage, 'setItem');
+    setItemSpy.and.callFake(() => {});
+
+    const mockNotesData: INote[] = [];
+
+    const getItemSpy = spyOn(localStorage, 'getItem');
+    getItemSpy.and.callFake(() => JSON.stringify(mockNotesData));
+
+    const expectedArgs = [LOCAL_STORAGE.notesData, JSON.stringify([mockNote])];
+
+    localStorageStrategy.create(mockNote).subscribe(() => {
+      expect(setItemSpy).toHaveBeenCalledWith(...expectedArgs);
+    });
+  });
+
+  it('should call localStorage.setItem, with appended notes, when create, with some loaded notes', () => {
+    const setItemSpy = spyOn(localStorage, 'setItem');
+    setItemSpy.and.callFake(() => {});
+
+    const mockLoadedNotes: INote[] = [
+      {
+        id: '0',
+        dateCreated: 1590045443712,
+        htmlText: '',
+        markdownText: '## Loaded notes',
+        title: 'Loaded Test Title',
+      },
+    ];
+
+    const getItemSpy = spyOn(localStorage, 'getItem');
+    getItemSpy.and.callFake(() => JSON.stringify(mockLoadedNotes));
+
+    const appendNotes = [...mockLoadedNotes, mockNote];
+    const expectedArgs = [LOCAL_STORAGE.notesData, JSON.stringify(appendNotes)];
+
+    localStorageStrategy.create(mockNote).subscribe(() => {
+      expect(setItemSpy).toHaveBeenCalledWith(...expectedArgs);
+    });
+  });
+
+  it('should throw errors, when create, if localStorage is full', () => {
+    const mockErrorMessage = 'LocalStorage is full!';
+    const setItemSpy = spyOn(localStorage, 'setItem');
+    setItemSpy.and.throwError(mockErrorMessage);
+
+    const getItemSpy = spyOn(localStorage, 'getItem');
+    getItemSpy.and.callFake(() => null);
+
+    localStorageStrategy.create(mockNote).subscribe(
+      () => {},
+      error => {
+        expect(error.message).toBe(mockErrorMessage);
+      },
+    );
+  });
+
+  it('should call localStorage.setItem, with modified note lists, when delete success', () => {
+    const mockDeleteId = '1';
+    const mockNotesData: INote[] = [mockNote];
+
+    const setItemSpy = spyOn(localStorage, 'setItem');
+    setItemSpy.and.callFake(() => {});
+
+    const getItemSpy = spyOn(localStorage, 'getItem');
+    getItemSpy.and.callFake(() => JSON.stringify(mockNotesData));
+
+    const expectedArgs = [LOCAL_STORAGE.notesData, JSON.stringify([])];
+
+    localStorageStrategy.delete(mockDeleteId).subscribe(() => {
+      expect(setItemSpy).toHaveBeenCalledWith(...expectedArgs);
+    });
+  });
+
+  it('should throw errors, when delete, if note list is already empty', () => {
+    const mockDeleteId = '1';
+    const setItemSpy = spyOn(localStorage, 'setItem');
+    setItemSpy.and.callFake(() => {});
+
+    const getItemSpy = spyOn(localStorage, 'getItem');
+    getItemSpy.and.callFake(() => null);
+
+    localStorageStrategy.delete(mockDeleteId).subscribe(
+      () => {},
+      error => {
+        expect(error.message).toBe(
+          'Unable to perform delete when there is no data!',
+        );
+      },
+    );
+  });
+
+  it('should call localStorage.setItem, with updated note lists, when update success', () => {
+    const mockTitleChange = 'This is an updated title';
+    const mockUpdateNoteData: Update<INote> = {
+      id: '1',
+      changes: {
+        title: mockTitleChange,
+      },
+    };
+    const mockNotesData: INote[] = [
+      mockNote,
+      {
+        id: '2',
+        dateCreated: 1590045443719,
+        htmlText: '',
+        markdownText: '## Some Note 2 Markdown',
+        title: 'Note no. 2',
+      },
+    ];
+
+    const setItemSpy = spyOn(localStorage, 'setItem');
+    setItemSpy.and.callFake(() => {});
+
+    const getItemSpy = spyOn(localStorage, 'getItem');
+    getItemSpy.and.callFake(() => JSON.stringify(mockNotesData));
+
+    const expectedUpdatedNoteList: INote[] = [
+      {
+        id: '1',
+        dateCreated: 1590045443715,
+        htmlText: '',
+        markdownText: '## Some Markdown',
+        title: mockTitleChange,
+      },
+      {
+        id: '2',
+        dateCreated: 1590045443719,
+        htmlText: '',
+        markdownText: '## Some Note 2 Markdown',
+        title: 'Note no. 2',
+      },
+    ];
+    const expectedArgs = [
+      LOCAL_STORAGE.notesData,
+      JSON.stringify(expectedUpdatedNoteList),
+    ];
+
+    localStorageStrategy.update(mockUpdateNoteData).subscribe(() => {
+      expect(setItemSpy).toHaveBeenCalledWith(...expectedArgs);
+    });
+  });
+
+  it('should throw errors, when update, if note list is already empty', () => {
+    const mockUpdateNoteData: Update<INote> = {
+      id: '1',
+      changes: {
+        title: 'Some changed title',
+      },
+    };
+    const setItemSpy = spyOn(localStorage, 'setItem');
+    setItemSpy.and.callFake(() => {});
+
+    const getItemSpy = spyOn(localStorage, 'getItem');
+    getItemSpy.and.callFake(() => null);
+
+    localStorageStrategy.update(mockUpdateNoteData).subscribe(
+      () => {},
+      error => {
+        expect(error.message).toBe(
+          'Unable to perform update when there is no data!',
+        );
       },
     );
   });
