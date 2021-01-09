5d2dfb52d72e9817840becbfbdfa617a0db325d6
Christian Ing Sunardi <christianingsunardi@gmail.com>
2021-01-09 11:25:21 +0700

    refactor: remove unnecessary entryComponents (v9)

--- src/app/shared/components/overlay-container/overlay-container.module.ts
+++ src/app/shared/components/overlay-container/overlay-container.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ContainerHostDirective } from '@app/shared/directives/container-host.directive';
-import { DialogComponent } from '../dialog/dialog.component';
 import { DialogModule } from '../dialog/dialog.module';
 import { OverlayContainerComponent } from './overlay-container.component';
 import { ToolbarDialogModule } from '../toolbar-dialog/toolbar-dialog.module';
-import { ToolbarDialogComponent } from '../toolbar-dialog/toolbar-dialog.component';
+
+import { ContainerHostDirective } from '@app/shared/directives/container-host.directive';
 
 @NgModule({
   declarations: [OverlayContainerComponent, ContainerHostDirective],
-  entryComponents: [DialogComponent, ToolbarDialogComponent],
   imports: [CommonModule, DialogModule, ToolbarDialogModule],
   exports: [OverlayContainerComponent],
 })
