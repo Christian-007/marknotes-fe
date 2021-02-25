c37d957d62d17b48499401cd22555b6aea41ef20
Christian Ing Sunardi <christianingsunardi@gmail.com>
2021-02-25 21:17:15 +0700

    style: add text cursor

--- src/app/shared/components/text-editor/text-editor.component.ts
+++ src/app/shared/components/text-editor/text-editor.component.ts
@@ -14,7 +14,7 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
   template: `
     <div
       #textEditor
-      class="outline-none font-mono text-xs text-gray-700 pb-4"
+      class="outline-none font-mono text-xs text-gray-700 pb-4 cursor-text"
       contenteditable="true"
       tabindex="1"
       role="textarea"
