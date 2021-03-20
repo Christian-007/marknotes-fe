49679ed457b7e732908b362b40e0b8b7eaa29d76
Christian Ing Sunardi <christianingsunardi@gmail.com>
2021-03-20 16:30:11 +0700

    refactor: remove fetchAllNotes() and unused codes

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,33 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
 import { INote } from '@app/shared/models/markdown-state.model';
-import { NotesActions } from '@app/shared/store/actions';
 import * as fromRoot from '@app/shared/store/reducers';
+import { NotesActions } from '@app/shared/store/actions';
 import { NotesSelector } from '@app/shared/store/selectors';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
 })
-export class CardComponent implements OnInit {
+export class CardComponent {
   notes$: Observable<INote[]>;
   loading$: Observable<boolean>;
   hasNotesInStorage$: Observable<boolean>;
-  buttonStyles: {};
 
   constructor(private store: Store<fromRoot.ApplicationState>) {
-    this.buttonStyles = {
-      padding: 0,
-    };
     this.notes$ = store.pipe(select(NotesSelector.selectAllNotes));
     this.loading$ = store.pipe(select(NotesSelector.selectNotesPending));
     this.hasNotesInStorage$ = store.pipe(select(fromRoot.hasNotesInStorage));
-  }
-
-  ngOnInit(): void {
-    this.store.dispatch(NotesActions.fetchAllNotes());
   }
 
   onClickAddNote(): void {
