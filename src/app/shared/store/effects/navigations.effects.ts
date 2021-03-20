fc8c9de09f4dc54a72432bfaf915c81ebe70df3b
Christian Ing Sunardi <christianingsunardi@gmail.com>
2021-03-20 17:36:37 +0700

    feat: navigate to '/notes' if note list is empty after deletion

--- src/app/shared/store/effects/navigations.effects.ts
+++ src/app/shared/store/effects/navigations.effects.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { switchMap, map, tap } from 'rxjs/operators';
+import { select, Store } from '@ngrx/store';
+import { EMPTY } from 'rxjs';
+import { switchMap, map, tap, withLatestFrom } from 'rxjs/operators';
 
-import { NavigationsActions } from '../actions';
+import { NavigationsActions, NotesActions } from '../actions';
+import * as fromRoot from '../reducers';
 
 import { NotesService } from '@app/pages/notes/notes.service';
 
@@ -13,6 +16,7 @@ export class NavigationsEffects {
     private actions$: Actions,
     private notesService: NotesService,
     private router: Router,
+    private store: Store<fromRoot.ApplicationState>,
   ) {}
 
   submitNoteTitle$ = createEffect(() =>
@@ -27,6 +31,22 @@ export class NavigationsEffects {
           );
       }),
     ),
+  );
+
+  navigateToNotes$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(NotesActions.deleteOneNoteSuccess),
+        withLatestFrom(this.store.pipe(select(fromRoot.hasNotesInStorage))),
+        tap(([_, hasNotesInStorage]) => {
+          if (!hasNotesInStorage) {
+            return this.router.navigate(['/notes']);
+          }
+
+          return EMPTY;
+        }),
+      ),
+    { dispatch: false },
   );
 
   navigateToNoteDetail$ = createEffect(
